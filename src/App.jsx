bccc7c4dd3615860567118f9a3a60cb43c4c9d93
Munachi <muachiukeme@gmail.com>
2026-01-28 16:10:44 +0100

    first comment

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import Component from "./Components/FirstComp"
+function App() {
+
+  return (
+    <div>
+      <Component/>
+    </div>
+      
+  
+  )
+}
+
+export default App
---
+++ src/Components/FirstComp.jsx
@@ -0,0 +1,17 @@
+// NOTES
+// Component is a reusable container of jsx codes.
+
+import React from "react";
+
+function Component (){
+    return(
+        <div>
+            <h1>REACT COMPONENT</h1>
+            <h2>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus sunt nobis cum, voluptas officia vel perspiciatis incidunt corporis laudantium obcaecati!
+            </h2>
+        </div>
+    );
+}
+
+export default Component;
